17145dab1772f07d9f7e375b3e92de246ee03825
SabbyG1 <jonathan.gavrilas@gmail.com>
2021-02-01 14:05:29 +0100

    Grafik

--- tetris_project/src/Grafik/Grid.java
+++ tetris_project/src/Grafik/Grid.java
@@ -1,4 +1,4 @@
-package grafik;
+package Grafik;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JLabel;
--- tetris_project/src/Grafik/Gui.java
+++ tetris_project/src/Grafik/Gui.java
@@ -1,4 +1,4 @@
-package grafik;
+package Grafik;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
