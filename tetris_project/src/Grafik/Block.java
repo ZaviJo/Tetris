822db344047be2e4d40b8501b41438e68ff85bb6
SabbyG1 <jonathan.gavrilas@gmail.com>
2021-02-04 16:13:51 +0100

    Update Block.java

--- tetris_project/src/Grafik/Block.java
+++ tetris_project/src/Grafik/Block.java
@@ -12,7 +12,105 @@ public class Block {
 	int x,y,groesse,rotation = 0;
 	int [][][] grenzen;				
 	Color color;
+	//boolean beweglich = true;
 	
+	public Block() {
+		typ = Blocktyp.zufall();
+		switch(typ) {
+		case I:
+			groesse = 4;
+			color = Color.magenta;
+			break;
+		case J:
+			groesse = 3;
+			color = Color.orange;
+			break;
+		case L:
+			groesse = 3;
+			color = Color.red;
+			break;
+		case O:
+			groesse = 2;
+			color = Color.yellow;
+			break;
+		case S:
+			groesse = 3;
+			color = Color.cyan;
+			break;
+		case T:
+			groesse = 3;
+			color = Color.green;
+			break;
+		case Z:
+			groesse = 3;
+			color = Color.blue;
+			break;
+		}
+		
+		x = 4;						//Mitte des Spielfeldes
+		y = -2;						//Block soll oberhalb des Spielfeldes starten.
+	}
+	
+	public void rotate() {
+		rotation++;
+	}
+	
+	public int[][][] blockBilder(Blocktyp typ) {
+		int [][][] grenzen = null;
+		switch (typ) {
+		case I:
+			int[][][] grenzen_I = { { { 0, 0, 0, 0 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } },
+					{ { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 } },
+					{ { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 } },
+					{ { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 1, 1, 1, 1 }, { 0, 1, 0, 0 } } };
+			grenzen = grenzen_I;
+			break;
+		case J:
+			int[][][] grenzen_J = { { { 1, 0, 0 }, { 1, 1, 1 }, { 0, 0, 0 } },
+					{ { 0, 1, 1 }, { 0, 1, 0 }, { 0, 1, 0 } }, 
+					{ { 0, 0, 0 }, { 1, 1, 1 }, { 0, 0, 1 } },
+					{ { 0, 1, 0 }, { 0, 1, 0 }, { 1, 1, 0 } } };
+			grenzen = grenzen_J;
+			break;
+		case L:
+			int[][][] grenzen_L = { { { 0, 0, 1 }, { 1, 1, 1 }, { 0, 0, 0 } },
+					{ { 0, 1, 0 }, { 0, 1, 0 }, { 0, 1, 1 } }, 
+					{ { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0 } },
+					{ { 1, 1, 0 }, { 0, 1, 0 }, { 0, 1, 0 } } };
+			grenzen = grenzen_L;
+			break;
+		case O:
+			int[][][] grenzen_O = { { { 1, 1 }, { 1, 1 } },
+					{ { 1, 1 }, { 1, 1 } }, 
+					{ { 1, 1 }, { 1, 1 } },
+					{ { 1, 1 }, { 1, 1 } } } ;
+			grenzen = grenzen_O;
+			break;
+		case S:
+			int[][][] grenzen_S = { { { 0, 1, 1 }, { 1, 1, 0 }, { 0, 0, 0 } },
+					{ { 0, 1, 0 }, { 0, 1, 1 }, { 0, 0, 1 } }, 
+					{ { 0, 0, 0 }, { 0, 1, 1 }, { 1, 1, 0 } },
+					{ { 1, 0, 0 }, { 1, 1, 0 }, { 0, 1, 0 } } };
+			grenzen = grenzen_S;
+			break;
+		case T:
+			int[][][] grenzen_T = { { { 0, 1, 0 }, { 1, 1, 1 }, { 0, 0, 0 } },
+					{ { 0, 1, 0 }, { 0, 1, 1 }, { 0, 1, 0 } }, 
+					{ { 0, 0, 0 }, { 1, 1, 1 }, { 0, 1, 0 } },
+					{ { 0, 1, 0 }, { 1, 1, 0 }, { 0, 1, 0 } } };
+			grenzen = grenzen_T;
+			break;
+		case Z:
+			int[][][] grenzen_Z = { { { 1, 1, 0 }, { 0, 1, 1 }, { 0, 0, 0 } },
+					{ { 0, 0, 1 }, { 0, 1, 1 }, { 0, 1, 0 } }, 
+					{ { 0, 0, 0 }, { 1, 1, 0 }, { 0, 1, 1 } },
+					{ { 0, 1, 0 }, { 1, 1, 0 }, { 1, 0, 0 } } };
+			grenzen = grenzen_Z;
+			break;
+		}
+		
+		return grenzen;
+	}
 	
 		
 
