9b57e2b850fc33fe115054b12462d48b2168df88
SabbyG1 <jonathan.gavrilas@gmail.com>
2021-02-04 14:41:41 +0100

    Blöcke

--- tetris_project/src/Grafik/Block.java
+++ tetris_project/src/Grafik/Block.java
@@ -1,5 +1,19 @@
 package Grafik;
 
+import java.awt.Color;				//Wird für die Farbauswahl eines Blockes verwendet
+
 public class Block {
+	/*In dieser Klasse werden die Blöcke gebildet. Jeder Block hat einen Typen, eine Position, Grösse
+	 *und Rotation. Jeder Block wird auch mit einem dreidimensionalem Array definiert, der als Informationen
+	 *die Rotation des Blockes und das Aussehen des Blockes beinhaltet. Jeder Block hat auch eine Farbe.
+	 * 
+	 */
+	Blocktyp typ;					
+	int x,y,groesse,rotation = 0;
+	int [][][] grenzen;				
+	Color color;
+	
+	
+		
 
 }
--- tetris_project/src/Grafik/Blocktyp.java
+++ tetris_project/src/Grafik/Blocktyp.java
@@ -1,21 +1,23 @@
 package Grafik;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ThreadLocalRandom; //Wird für die Auswahl eines zufälligen Blockes verwendet.
 
 public enum Blocktyp {
 	/*Jeder entstehende Block kann von der Form ein I,J,L,O,S,T,Z sein. Daher ist es von Vorteil
 	 *Enumerations zu verwenden, da sie eine Spezialklasse ist, die eine Gruppe von Konstanten
 	 *repräsentiert.
+	 *In dieser Klasse werden die Blocktypen definiert und ein zufälliger Blocktyp gewählt.
 	 */
 	I,J,L,O,S,T,Z;
 	
-	public static Blocktyp random() {
+	public static Blocktyp zufall() {
 		/*Dies ist eine Methode, um einen zufälligen Block zu erstellen, der in dieser Klasse ist.
-		 *Die Methode gibt ein ... mit dem Datentyp Blocktyp zurück.
+		 *Die Methode gibt ein Array mit dem Datentyp Blocktyp zurück.
 		 */
 		return values()[ThreadLocalRandom.current().nextInt(0,values().length)];
-		/*
-		 * 
+		/*Die Methode values() liefert ein Array, bestehend aus den 7 Konstanten der Klasse Blocktyp.
+		 * ThreadLocalRandom.current() kreiert eine Zufallszahl zwischen 0 bis values.length-1. 
+		 * In unserer Klasse Blocktyp können diese Zahlen 0,1,2,3,4,5,6 sein, da wir 7 Elemente haben.
 		 */
 	}
 }
