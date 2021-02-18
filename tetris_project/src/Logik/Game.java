979c5e9a4b0ae2457d021deb7e16ee3e0a2a9036
SabbyG1 <jonathan.gavrilas@gmail.com>
2021-02-18 14:23:01 +0100

    Update Game.java

--- tetris_project/src/Logik/Game.java
+++ tetris_project/src/Logik/Game.java
@@ -1,8 +1,7 @@
 package Logik;
 
-import java.util.ArrayList;
-
-import Grafik.Block; // ??? vllt kei ahnig
+import java.util.ArrayList;		//Wird für die Erstellung von ArrayLists benötigt. 
+import Grafik.Block; 			//Wird benötigt, um die vorbereiteten Blöcke zu verwenden. 
 
 public class Game {
 	/*Hier befinden sich die Grundvariablen für das Spiel. 
@@ -14,16 +13,18 @@ public class Game {
 	public static boolean SpawnNeuerBlock = false;		//Variable ist für die Erstellung eines neuen Blockes notwendig, wenn true wird ein neuer erstellt.
 	public static boolean schneller = false;			//Wenn man die Pfeiltaste nach unten drückt, beschleunigt sich der Block
 	
-	public static ArrayList<Block> blocks = new ArrayList<Block>(); 
-	public static Block aktuellerBlock, naechsterBlock;
-	public static int [][] map = new int [10][18];
+	public static ArrayList<Block> blocks = new ArrayList<Block>(); 	//ArrayLists haben den Vorteil, dass man sie einfach erweitern und bearbeiten kann.
+	public static Block aktuellerBlock, naechsterBlock;					//Der aktuelle Block und der nächste Block, der in kleinesGrid angezeigt wird.
+	public static int [][] map = new int [10][18];						//Zweidimensionales Array für das Spielfeld. Blöcke sollen in diesem Array dargestellt werden.
 	
 	public static Zustand zustand = Zustand.start;
 	
 	public static void zurücksetzen() {
-		for(int x =0; x < 10; x++) {    //wenn nicht gehen bitte x<map.length anstatt 10
+		/*Die Methode setzt die aktuelle Map zurück. Wird bei einem Spielneustart verwendet.
+		 */
+		for(int x =0; x < 10; x++) {    	//wenn nicht gehen bitte x<map.length anstatt 10
 			for (int y = 0; y< 18; y++) {	//gleich einf mit y<map[x].length
-				map[x][y] =0;
+				map[x][y] =0;			//Diese Schleife setzt alle Felder im Array map wieder auf 0.
 			}
 		}
 		score = 0;
