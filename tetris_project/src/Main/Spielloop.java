aeab934d66e1063088d545458a92c965a1ca244b
KleinZyanid <78206111+KleinZyanid@users.noreply.github.com>
2021-02-18 16:14:32 +0100

    test 6

--- tetris_project/src/Main/Spielloop.java
+++ tetris_project/src/Main/Spielloop.java
@@ -1,30 +1,23 @@
 package Main;
 
-import Grafik.Block;		//Wichtig für den Zugriff auf 
-import Logik.Game;			//Wird benötigt, um auf die Instanzvariablen der Klasse Game zugreifen zu können.
-import Logik.Zustand;		//
+import Grafik.Block;
+import Logik.Game;
+import Logik.Zustand;
 
 public class Spielloop extends Thread{
-	/*Unsere Spielloop ist eine Erweiterung der Klasse Thread. Threads werden benutzt, um gleichzeitig mehrere Operationen
-	 * effizient durchführen zu können. Hier wird das Spiel dauernd aktualisiert.
-	 */
-	private boolean laufend = true;		//Wird verwendet, wenn man das Programm unterbrechen will. Abbruchbedingung.		
+	private boolean laufend = true;			
 	
+	//@Override //vllt ka
 	public void run() {				
-	/*Methode run() wird überschrieben, um unser eigenes Programm einfügen zu können. 
-	 */
+		
 		while(laufend) {
 			try {
-<<<<<<< HEAD
 				if(Game.zustand == Zustand.ingame) {
-=======
-				if(Game.zustand == Zustand.start) {	//Wird ausgeführt, wenn der Zustand des Spiels start ist. Nur dann werden Sachen generiert.
->>>>>>> adcf2c721d2152f368913076c4266d7053ccab9c
 					
-					Game.aktuellerBlock.setY(Game.aktuellerBlock.getY()+1); 
+					Game.aktuellerBlock.setY(Game.aktuellerBlock.getY()+1);
 					
-					if(Game.SpawnNeuerBlock) { 							//Wenn ein neuer Block erstellt werden muss. Hier: SpawnNeuerBlock = true.
-						Game.blocks.add(Game.naechsterBlock);			//naechsterBlock wird der ArrayList hinzugefügt.
+					if(Game.SpawnNeuerBlock) {
+						Game.blocks.add(Game.naechsterBlock);
 						Game.aktuellerBlock = Game.naechsterBlock;
 						Game.naechsterBlock = new Block();
 						Game.SpawnNeuerBlock = false;
